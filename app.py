60ab1f250b143389cabda2fb7711bd35ba83be3d
Logan Gall <141678252+logan-gall@users.noreply.github.com>
2024-02-08 14:04:13 -0600

    update app

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def get_polygon_geojson():
     conn.close()
 
     # Return the GeoJSON
-    return row
+    return jsonify(row)
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))

57fb95056deaf284a973f9f12d2ab249fd4137e9
Logan Gall <gall0487@umn.edu>
2024-04-04 14:34:19 -0500

    add final map

--- app.py
+++ app.py
@@ -291,7 +291,7 @@ def delta_cost_map():
                         'features', json_agg(
                             json_build_object(
                                 'type', 'Feature',
-                                'geometry', ST_AsGeoJSON(ST_Transform(ST_SetSRID(shape, 26915), 4326))::json,
+                                'geometry', ST_AsGeoJSON(ST_Transform(ST_SetSRID(geom, 26915), 4326))::json,
                                 'properties', json_build_object(
                                     'delta_cost', delta_cost
                                 )
